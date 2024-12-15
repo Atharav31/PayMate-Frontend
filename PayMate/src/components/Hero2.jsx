6a1f64bda3a434b939aa0f3128527538a1db005f
Atharav Uttekar <atharavuttekar@gmail.com>
2024-12-15 17:21:25 +0530

    features

--- PayMate/src/components/Hero2.jsx
+++ PayMate/src/components/Hero2.jsx
@@ -1,7 +1,32 @@
 import { motion } from "framer-motion";
-import feature2 from "../assets/feature2.jpg";
+import image1 from "../assets/feature1.jpg";
+import image2 from "../assets/feature2.jpg";
 
 const Hero2 = () => {
+  const features = [
+    {
+      image: image1,
+      title: " Expense Tracking",
+      description: "Add expenses easily and assign them to group members.",
+    },
+    {
+      image: image2,
+      title: "Fair Bill Splitting",
+      description: "Automatically calculates how much each person owes.",
+    },
+    {
+      image: image2,
+      title: "No Sign-Up ",
+      description:
+        "Start using PayMate instantly without needing to create an account.",
+    },
+    {
+      image: image2,
+      title: "Free to Use",
+      description: "No charges or subscriptions—PayMate is completely free.",
+    },
+  ];
+
   return (
     <>
       <div
@@ -13,30 +38,24 @@ const Hero2 = () => {
         }}
       >
         <div className="hero-content grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 p-6">
-          {Array(4)
-            .fill("")
-            .map((_, index) => (
-              <motion.div
-                key={index}
-                initial={{ opacity: 0, y: 50 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, delay: index * 0.2 }}
-                viewport={{ once: false, amount: 0.5 }} // Trigger animations every time section enters view
-                className="feature-card flex flex-col items-center"
-              >
-                <img
-                  src={feature2}
-                  className="max-w-sm rounded-lg shadow-2xl mb-4"
-                  alt={`Feature ${index + 1}`}
-                />
-                <h1 className="text-3xl font-bold">Feature {index + 1}</h1>
-                <p className="py-4 text-center">
-                  Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                  assumenda excepturi exercitationem quasi. In deleniti eaque
-                  aut repudiandae et a id nisi.
-                </p>
-              </motion.div>
-            ))}
+          {features.map(({ image, title, description }, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: index * 0.2 }}
+              viewport={{ once: false, amount: 0.5 }} // Trigger animations every time section enters view
+              className="feature-card flex flex-col items-center"
+            >
+              <img
+                src={image}
+                className="max-w-sm rounded-lg shadow-2xl mb-4"
+                alt={`Feature ${index + 1}`}
+              />
+              <h1 className="text-3xl font-bold text-center">{title}</h1>
+              <p className="py-4 text-center">{description}</p>
+            </motion.div>
+          ))}
         </div>
       </div>
     </>
